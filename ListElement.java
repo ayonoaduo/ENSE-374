5f126dbf120bbc56bc12d92ea02a18702ca80361
Onisokien Ayonoadu <ayonoaduo@gmail.com>
2019-10-02 16:15:07 -0600

    Commit that changed mistake of implementing double instead of single

--- ListElement.java
+++ ListElement.java
@@ -1,13 +1,13 @@
 public class ListElement {
   private ListElement next;
-  private ListElement previous;
+  //private ListElement previous;
   private int data;
 
   public ListElement ()
   {
     this.data = 0;
     this.next = null;
-    this.previous = null ;
+    //this.previous = null ;
   }
 
   public setData(int data)
