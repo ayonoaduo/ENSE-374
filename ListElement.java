c95c96de30e60505c861509496839c024f7da0bb
Onisokien Ayonoadu <ayonoaduo@gmail.com>
2019-10-02 16:03:37 -0600

    Node

---
+++ ListElement.java
@@ -0,0 +1,21 @@
+public class ListElement {
+  private ListElement next;
+  private ListElement previous;
+  private int data;
+
+  public ListElement ()
+  {
+    this.data = 0;
+    this.next = null;
+    this.previous = null ;
+  }
+
+  public setData(int data)
+  {
+    this.data=data;
+  }
+  public getData()
+  {
+    return this.data;
+  }
+}
